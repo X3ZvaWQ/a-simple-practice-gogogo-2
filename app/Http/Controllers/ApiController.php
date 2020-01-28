093d0fac2faaddf3623143ee7add5e608f66fe23
conquer-eden <2720512973@qq.com>
2020-01-28 18:12:14 +0800

    mmm

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -7,8 +7,13 @@ use Illuminate\Http\Request;
 class ApiController extends Controller
 {
     // API - 例 的控制器部分
-    public function hello (Request $request)
+    public function hello() 
+
     {
-        return $request->ip();
+        return response()->json([
+            'ret' => 200,
+            'desc' => 'success',
+            'data' => '哎呀api-III我都把百度搜烂了也不会所以就摸鱼了，有时间一定继续钻研咕咕咕咕（x'
+        ]);        
     }
-}
+}    
