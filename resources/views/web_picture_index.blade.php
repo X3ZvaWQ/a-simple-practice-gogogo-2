920bdbf9d128c7ecc05a33a0d1fe523e3485ab42
WORTY486 <1084301506@qq.com>
2020-02-04 19:58:45 +0800

    修正

--- resources/views/web_picture_index.blade.php
+++ resources/views/web_picture_index.blade.php
@@ -5,29 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>web照片墙</title>
-    <style type="text/css">
-     *{
-    padding: 0;
-    margin: 0;    
-    }
-    .image{
-    width: 660px;
-    height: 660px;
-    margin:0px auto;
-    }
-    li{
-        list-style: none;
-        float: left;
-        width: 200px;
-        height: 200px;
-        margin: 10px;
-    }
-    </style>
 </head>
 <body>
     <div class="image">
     @foreach ($href as $qq)
-    <li><img src="{{ $qq }}"></li>
+    <img src="{{ $qq->href }}">
     @endforeach
     </div>
 </body>
